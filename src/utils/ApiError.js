76d43da0ec1438c07dfdd0b174eabc33390cff16
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-10-13 22:28:24 +0530

    feat: added apiError class

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error{
+
+  constructor(
+    statusCode,
+    message="something went wrong",
+    errors=[],
+    stack =""
+  ){
+    super(message),
+    this.statusCode = statusCode,
+    this.errors = errors,
+    this.data=null,
+    this.message = message,
+    this.sucess= false
+
+
+    if(stack){
+      this.stack = stack
+    }else{
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+export {ApiError}
